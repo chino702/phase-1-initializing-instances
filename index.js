94da0374a590ca6c106add1bbcc736f440f923b1
chino702 <chinox3702@gmail.com>
2023-04-30 16:55:04 -0700

    Initializing instances

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+
+  class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    }
+
+    get courses() {
+        return [this.salad, this.soup, this.entree, this._dessert];
+    }
+
+    set courses(courses) {
+        this.salad = courses[0];
+        this.soup = courses[1];
+        this.entree = courses[2];
+        this._dessert = courses[3];
+    }
+}
